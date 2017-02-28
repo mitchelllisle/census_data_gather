7400d0218683a81e015a644b7651a3699e9a151f
Mitchell <mitchell.lisle@bigdatr.com>
2017-02-28 23:24:02 +1100

    Create census_data_gather.r

---
+++ census_data_gather.r
@@ -0,0 +1,6 @@
+census_reshape <- function(read_file, write_file, id){
+  x <- read.csv(read_file, check.names = FALSE)
+  molten = melt(x, id = id, na.rm = FALSE)
+  data.frame(molten)
+  write.csv(molten, write_file, row.names = FALSE)
+}
